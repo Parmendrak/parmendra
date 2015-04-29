815991ba4d45c1dac602da0ab4c5916c233b9561
360_macmini <parmendrak@360logica.com>
2015-04-29 22:37:35 +0530

    PK

--- Android2/MainActivity.cs
+++ Android2/MainActivity.cs
@@ -26,7 +26,7 @@ namespace Android2
 			Button button = FindViewById<Button> (Resource.Id.myButton);
 
 			Console.WriteLine ("Parmendra");
-			Console.WriteLine ("Arpit");
+
 
 			button.Click += delegate {
 				button.Text = string.Format ("{0} clicks!", count++);
