56dfdda244b8eb9f820d7d1de3a6ef7cffa3cdac
Parmendrak <parmendrak@360logica.com>
2015-04-30 18:47:09 +0530

    Update MainActivity.cs

--- Android2/MainActivity.cs
+++ Android2/MainActivity.cs
@@ -26,7 +26,7 @@ namespace Android2
 			Button button = FindViewById<Button> (Resource.Id.myButton);
 
 			Console.WriteLine ("ParmendraChoudhary");
-
+                        Console.WriteLine ("PKDK");
 			button.Click += delegate {
 				button.Text = string.Format ("{0} clicks!", count++);
 			};
